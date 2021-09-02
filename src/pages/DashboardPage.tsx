5a0e4ffb22c5d5e697e7a26cb1c404d5f5cadd1f
Alex Ilushyk <ailushyk@gmail.com>
2021-09-03 01:40:26 +0200

    link to app2

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,16 @@
 import React, { FC } from 'react';
+import { Action } from 'ui/elements';
+import { Link } from 'react-router-dom';
 
-const DashboardPage: FC = () => <div>Dashboard Page</div>;
+const DashboardPage: FC = () => (
+  <div>
+    Dashboard Page
+    <div>
+      <Action.Link as={Link} to={'app2'}>
+        go to app2
+      </Action.Link>
+    </div>
+  </div>
+);
 
 export default DashboardPage;
