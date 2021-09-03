1c910d942e21e2e1d81dc78eddf3af3e480a20b6
Alex Ilushyk <ailushyk@gmail.com>
2021-09-03 16:11:24 +0200

    add types

---
+++ src/pages/PageTest.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+import WelcomePage from 'ui/pages/WelcomePage';
+
+const PageTest: FC = () => <WelcomePage header={'test page '} />;
+
+export default PageTest;
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,4 @@ declare module 'app2/components';
 declare module 'ui/elements';
 declare module 'ui/components';
 declare module 'ui/pages';
+declare module 'ui/pages/WelcomePage';
