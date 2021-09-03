4486d91a85987f685bc0d075b7f53005c87d67bf
Alex Ilushyk <ailushyk@gmail.com>
2021-09-03 10:27:21 +0200

    add links

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,14 +1,29 @@
 import React, { FC } from 'react';
-import { Action, Space } from 'ui/elements';
-import { Layout } from 'ui/components';
+import { Action, Space, Text } from 'ui/elements';
+import { Header, Layout } from 'ui/components';
 import { Link } from 'react-router-dom';
 
 const DashboardPage: FC = () => (
   <Layout header={'Dashboard Page'}>
     <Space>
-      <Action.Link as={Link} to={'app2'}>
-        go to app2
-      </Action.Link>
+      <Header>Choice your app</Header>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae
+        consequatur doloribus ducimus, eos esse fugiat id minima omnis
+        perspiciatis quod vero voluptatibus.
+      </Text>
+      <Space m={12}>
+        <Space p={'4'} flex>
+          <Action.Link as={Link} to={'app1'}>
+            go to app 1
+          </Action.Link>
+        </Space>
+        <Space p={'4'} flex>
+          <Action.Link as={Link} to={'app2'}>
+            go to app 2
+          </Action.Link>
+        </Space>
+      </Space>
     </Space>
   </Layout>
 );
